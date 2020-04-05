3e4a2811f47c7020a215bfc05ba0ac92b747950d
fikialamsyah <fikialamsyah@github.com>
2020-04-05 23:53:44 +0700

    menambahkan folder latihan 02 - Variabel, Assignment dan Deklarasi

---
+++ 02 - Variabel, Assignment dan Deklarasi/src/com/tutorial/Main.java
@@ -0,0 +1,17 @@
+package com.tutorial;
+
+public class Main {
+    public static void main(String[] args){
+
+        // Kita akan membuat variabel
+        // Tipe data
+        int a = 10; // Assigment
+        System.out.println("Nilai X adalah = " + a);
+
+        // Kita akan membuat sebuah deklarasi
+        int b; // Deklrasi
+        b = 8;
+        System.out.println("Nilai Y adalah = " + b);
+
+    }
+}
