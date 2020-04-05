383a46ad8f3896685ce130e658c2581b72937c9e
fikialamsyah <fikialamsyah@github.com>
2020-04-05 23:51:19 +0700

    menambahkan folder latihan 01- Print dan Jalur Eksekusi

---
+++ 01 - Print dan Jalur Eksekusi/src/com/tutorial/Main.java
@@ -0,0 +1,25 @@
+package com.tutorial;
+
+public class Main {
+    public static void main(String[] args){
+
+        System.out.println("Hello gaes");
+        System.out.println("Baris ke-2 : Hai mail");
+
+        System.out.print("Baris ke-3 : Ini adalah baris ketiga");
+        System.out.print("Baris ke-4 : saya meneruskan baris ke empat");
+
+        System.out.print("Baris ke-5 : Hallo meymey???");
+        System.out.println("Baris ke-6 : Hallo juga mail!!!");
+
+        System.out.print("Baris ke-7 : Hai bro, mari kita cabut\n");
+        System.out.print("Baris ke-8 : Cabut kemana brow \n");
+        System.out.print("Baris ke-9 : Mari kita cabut ke rumah tuk dalang!!!\n");
+
+        System.out.printf("Nomor plat valentina rosi %d",46);
+
+
+
+
+    }
+}
